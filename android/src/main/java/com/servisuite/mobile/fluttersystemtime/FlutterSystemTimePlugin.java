e616c2eda658c1f6ba5be66816d18b016e0744e0
ibarrick <ianbarrick@gmail.com>
2021-02-22 14:00:38 -0500

    migrate to v2 embedding

--- android/src/main/java/com/servisuite/mobile/fluttersystemtime/FlutterSystemTimePlugin.java
+++ android/src/main/java/com/servisuite/mobile/fluttersystemtime/FlutterSystemTimePlugin.java
@@ -2,6 +2,8 @@ package com.servisuite.mobile.fluttersystemtime;
 
 import android.os.SystemClock;
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -9,11 +11,22 @@ import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** FlutterSystemTimePlugin */
-public class FlutterSystemTimePlugin implements MethodCallHandler {
+public class FlutterSystemTimePlugin implements FlutterPlugin, MethodCallHandler {
   /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_system_time");
     channel.setMethodCallHandler(new FlutterSystemTimePlugin());
+  }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding binding) {
+    final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_system_time");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+
   }
 
   @Override
